b2897adae186769a4af657b3994fbd8e0b104264
endervos <psgfc1282004@gmail.com>
2025-09-23 23:56:17 +0700

    Update code sign up

--- src/main/java/demoweb/demo/security/SecurityConfiguration.java
+++ src/main/java/demoweb/demo/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package demoweb.demo.security;
 
 import demoweb.demo.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -31,7 +30,6 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(configurer -> configurer
                         .requestMatchers(
                                 "/**",
-                                "/page-fault/**",
                                 "/sign-up/**",
                                 "/Customer/**"
                         ).permitAll()
