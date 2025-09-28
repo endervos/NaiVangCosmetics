33d4340d7dd31b3223f3955251577d88e2cf5966
LamThuyenNguyen <nguyenngoclamthuyen0977499533@gmail.com>
2025-09-28 22:59:39 +0700

    be profile

--- src/main/java/demoweb/demo/service/UserService.java
+++ src/main/java/demoweb/demo/service/UserService.java
@@ -1,7 +1,21 @@
 package demoweb.demo.service;
 
+import demoweb.demo.entity.User;
+import demoweb.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
 }
